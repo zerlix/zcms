9c9761a1a21e5e76a2bc2cc5afb7fb0d3fc3bc7b
Zerlix <t.hemm@gmx.de>
2025-07-28 12:06:12 +0200

    Laravel Breeze Authentifizierung hinzugefügt
    - Vollständiges Authentifizierungssystem mit Login/Registrierung installiert
    - Geschützte Admin-Routen für Seitenverwaltung implementiert
    - Benutzerregistrierung, Login, Logout-Funktionalität hinzugefügt
    - Profilverwaltung und Passwort-Reset Features integriert
    - Tailwind CSS für UI-Styling konfiguriert
    - Original app.blade.php als app.blade.old.php gesichert
    - /pages Admin-Bereich mit Auth-Middleware geschützt

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Resource route for PageController
-Route::resource('pages', \App\Http\Controllers\PageController::class);
-
-// Slug-Route NACH der Resource-Route
+// Öffentliche Seite (Slug-Route)
 Route::get('pages/{slug}', [\App\Http\Controllers\PageController::class, 'show'])->name('pages.show');
 
+// GESCHÜTZTE Admin-Seiten (NUR mit Login!)
+Route::middleware('auth')->group(function () {
+    Route::resource('pages', \App\Http\Controllers\PageController::class)->except(['show']);
+    
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Dashboard
+Route::get('/dashboard', function () {
+    return redirect()->route('pages.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
