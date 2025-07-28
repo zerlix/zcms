a517511ed8c94ec29c2daf00b8c0d6a1bbff585a
Zerlix <t.hemm@gmx.de>
2025-07-28 09:26:22 +0200

    Ressourcen-Routen und Blade-View-Struktur für Seiten hinzugefügt
    - Fügt die Resource-Route für den PageController hinzu
    - Legt das Verzeichnis und die Grundstruktur für die Seiten-Views an

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Resource route for PageController
+Route::resource('pages', \App\Http\Controllers\PageController::class);
